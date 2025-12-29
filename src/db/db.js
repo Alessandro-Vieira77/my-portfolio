d855c641d55b0c073b865dee5867b52c4e3edaf2
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-12-29 16:31:12 -0300

    chore: db update

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    name: 'Fin Track',
+    name: 'FinTrack',
     image: 'https://i.imgur.com/KhCqZyA.png',
     projectContent: {
       gallery: [
@@ -9,14 +9,28 @@ export const projects = [
         'https://i.imgur.com/qBAUf2p.png',
       ],
       description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in",
+        'I created FinTrack, an intuitive dashboard that helps users organize their financial lives. The tool allows users to record income, expenses, and investments, save and edit each transaction by date, and view their complete history in a simple and practical way. The goal of the project is to provide clarity, autonomy, and financial control through a user-friendly interface designed to support better decision-making.',
       objectives:
         'Gerenciamento de finanças pessoais para pessoas que estão começando a economizar e investir.',
-      technologies: ['React', 'Node.js', 'MongoDB', 'Tailwind CSS', 'TypeScript'],
+      technologies: [
+        'Vite',
+        'React',
+        'JavaScript',
+        'Tailwind CSS',
+        'Shadcn UI',
+        'Tanstack query',
+        'React router',
+        'React hook form',
+        'Zod',
+        'Axios',
+        'Date fns',
+        'React Number Format',
+        'Query String',
+      ],
       challengesEndSolutions:
         'Fin Track é uma plataforma que ajuda as pessoas a gerenciar suas finanças pessoais.',
-      repository: 'https://github.com/your-username/fin-track',
-      deploy: 'https://fin-track.vercel.app',
+      repository: 'https://github.com/Alessandro-Vieira77/fintrack.git',
+      deploy: 'https://fintrack-gules.vercel.app/',
     },
     categorys: ['Project', 'Design', 'Code', 'Full Stack'],
   },
